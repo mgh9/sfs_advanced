7675c1feb81e3ae084133fa07c4b9341844247e5
ghardashpoor-m <mahdi.ghardashpoor@gmail.com>
2024-07-27 12:32:31 +0330

    fix: resolve validations on AddProduct command

--- src/SFSAdv.Application/Products/Commands/AddProduct/AddProductCommandValidator.cs
+++ src/SFSAdv.Application/Products/Commands/AddProduct/AddProductCommandValidator.cs
@@ -2,14 +2,20 @@
 
 namespace SFSAdv.Application.Products.Commands.AddProduct;
 
-public class CreateCustomerCommandValidator : AbstractValidator<AddProductCommand>
+public class AddProductCommandValidator : AbstractValidator<AddProductCommand>
 {
-    public CreateCustomerCommandValidator()
+    public AddProductCommandValidator()
     {
         RuleFor(x => x.Title)
             .MaximumLength(40);
 
         RuleFor(x => x.Price)
-            .NotEmpty();
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(x => x.Discount)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(x => x.InventoryCount)
+            .GreaterThanOrEqualTo(0);
     }
 }
